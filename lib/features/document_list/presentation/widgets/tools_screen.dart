4589a4c216466eaf1abf9be54c8d2955b36f7f05
nsharipov <nikorurk@icloud.com>
2025-02-17 00:30:53 +0500

    chore: polishing

--- lib/features/document_list/presentation/widgets/tools_screen.dart
+++ lib/features/document_list/presentation/widgets/tools_screen.dart
@@ -11,12 +11,12 @@ class ToolsScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.background,
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(AppDimensions.paddingM),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(AppDimensions.paddingM),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
@@ -35,6 +35,71 @@ class ToolsScreen extends StatelessWidget {
                     onPressed: () {},
                   ),
                 ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.all(AppDimensions.paddingM),
+                children: [
+                  _buildToolItem(
+                    icon: Icons.document_scanner,
+                    title: AppStrings.scan,
+                    onTap: () {},
+                  ),
+                  SizedBox(height: AppDimensions.spacingM),
+                  _buildToolItem(
+                    icon: Icons.photo_library,
+                    title: AppStrings.gallery,
+                    onTap: () {},
+                  ),
+                  SizedBox(height: AppDimensions.spacingM),
+                  _buildToolItem(
+                    icon: Icons.folder_open,
+                    title: AppStrings.files,
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildToolItem({
+    required IconData icon,
+    required String title,
+    required VoidCallback onTap,
+  }) {
+    return Material(
+      color: AppColors.cardBackground,
+      borderRadius: BorderRadius.circular(AppDimensions.borderRadiusM),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(AppDimensions.borderRadiusM),
+        child: Padding(
+          padding: EdgeInsets.all(AppDimensions.paddingM),
+          child: Row(
+            children: [
+              Icon(
+                icon,
+                size: AppDimensions.iconM,
+                color: AppColors.buttonBlue,
+              ),
+              SizedBox(width: AppDimensions.spacingM),
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: AppDimensions.bodySize,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Spacer(),
+              Icon(
+                Icons.arrow_forward_ios,
+                size: AppDimensions.iconS,
+                color: AppColors.textSecondary,
               ),
             ],
           ),
